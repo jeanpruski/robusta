abc93c73791bfd31b3f5137d2ccfd2a422d61d1a
Prjski <jean.pruski@gmail.com>
2024-02-25 21:57:47 +0100

    init electron project

---
+++ src/components/GlobalLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface GlobalLayoutProps {
+}
+
+const GlobalLayout: React.FC<GlobalLayoutProps> = ({ }) => {
+  return (
+    <div>
+     yes
+    </div>
+  );
+};
+
+export default GlobalLayout;
